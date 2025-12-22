960bf52b76536e971a96307630c37c5bbdcbeea2
Kaleab Bantayehu <kaleabbantayehu@gmail.com>
2025-12-22 19:45:44 +0300

    Add underwater fog and match background color

--- src/core/scene.js
+++ src/core/scene.js
@@ -1,6 +1,7 @@
 import * as THREE from "../../lib/three.module.js";
 export function createScene() {
   const scene = new THREE.Scene();
-  scene.background = new THREE.Color(0x0b132b);
+  scene.fog = new THREE.Fog(0x0a1a2a, 5, 25);
+  scene.background = new THREE.Color(0x0a1a2a);
   return scene;
 }
